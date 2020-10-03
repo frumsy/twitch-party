0ac4c86443b32fb21779b9f4ec8b49c4a59870f1
frumsy <liambritt3@gmail.com>
2020-10-03 13:16:48 -0400

    Updates

---
+++ precache-manifest.af63511f3547b5e3db3b329e610b65ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740c885516a83c1004bed1a9b87ea6d1",
+    "url": "/twitch-party/index.html"
+  },
+  {
+    "revision": "1770517a6c0529f4c9b7",
+    "url": "/twitch-party/static/css/main.3af2657f.chunk.css"
+  },
+  {
+    "revision": "15e1495163f17081816d",
+    "url": "/twitch-party/static/js/2.94f9b077.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/twitch-party/static/js/2.94f9b077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1770517a6c0529f4c9b7",
+    "url": "/twitch-party/static/js/main.cbf4fba3.chunk.js"
+  },
+  {
+    "revision": "925ebac21d6ea4cb63ca",
+    "url": "/twitch-party/static/js/runtime-main.f94e88bb.js"
+  }
+]);
